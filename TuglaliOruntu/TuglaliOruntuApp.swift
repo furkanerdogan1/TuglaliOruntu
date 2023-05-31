2e153446f2adbb362b9e94de7a5ee6ee29db4c24
Furkan Erdoğan <edorfs.fe@gmail.com>
2023-05-31 13:35:19 +0300

    Initial Commit

---
+++ TuglaliOruntu/TuglaliOruntuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuglaliOruntuApp.swift
+//  TuglaliOruntu
+//
+//  Created by Furkan Erdoğan on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TuglaliOruntuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
