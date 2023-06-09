d48ddbac87f145b98f2283af3440dcbe7584223d
Furkan <edorfs.fe@gmail.com>
2023-06-09 15:43:06 +0300

    Model Created

---
+++ Models/QuestionModel.swift
@@ -0,0 +1,32 @@
+//
+//  QuestionModel.swift
+//  TuglaliOruntu
+//
+//  Created by Furkan Erdoğan on 9.06.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct Question {
+    var currentQuestionIndex: Int
+    var quizModel: QuestionModel
+    var quizCompleted: String = "start"
+    var quizWinningStatus: Bool = false
+}
+
+struct QuestionModel {
+    var question: String
+    var answer: String
+    var optionsList: [QuizOption]
+}
+
+struct QuestionOption : Identifiable {
+    var id: Int
+    var optionId: String
+    var option: String
+    var color: Color
+    var isSelected : Bool = false
+    var isMatched : Bool = false
+}
+
